79b8059e34dcb3871003ebfb7f58c9b5c26b5e97
Zach <lovelaza@gmail.com>
2024-02-09 01:07:45 -0600

    ZL

---
+++ src/main/java/frc/robot/Subsystems/SwerveModule.java
@@ -0,0 +1,13 @@
+package frc.robot.Subsystems;
+
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.revrobotics.CANSparkMax;
+
+public class SwerveModule {
+   private final CANSparkMax driveMotor;
+   private final CANSparkMax angleMotor;
+
+   private final CANcoder driveMotor;
+   private final CANcoder angleMotor;
+   
+}
