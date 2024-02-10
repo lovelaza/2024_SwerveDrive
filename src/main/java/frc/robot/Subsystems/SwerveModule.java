e7787e9b4ddade73d337403f42c7280b34159c5a
Zach <lovelaza@gmail.com>
2024-02-09 19:49:03 -0600

    ZL

--- src/main/java/frc/robot/Subsystems/SwerveModule.java
+++ src/main/java/frc/robot/Subsystems/SwerveModule.java
@@ -4,15 +4,14 @@ import com.ctre.phoenix6.hardware.CANcoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.wpilibj.PneumaticsControlModule;
-
 
 public class SwerveModule {
 
    private final CANSparkMax m_driveMotor;
    private final CANSparkMax m_turnMotor;
    private final RelativeEncoder m_driveEncoder;
+
+   //Do I need the relativeEncoder and the CANcoder or just one?
    private final RelativeEncoder m_turnEncoder;
    private final CANcoder m_cancoder;
 
